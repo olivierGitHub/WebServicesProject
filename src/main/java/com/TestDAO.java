918986a0abdaea3908402cb1d225c075f03fe39b
OlivierGitHub <olivier_coulibaly@yahoo.fr>
2015-03-01 01:23:20 +0100

    Ajout Update et Delete

--- src/main/java/com/TestDAO.java
+++ src/main/java/com/TestDAO.java
@@ -10,25 +10,25 @@ import com.dataAccess.dao.RoomDAO;
 import java.util.Date;
 
 /**
- * Created by oliver on 27/02/15.
+ * Created by oliver on 01/03/15.
  */
-public class Main {
+public class TestDAO {
 
     public static void main (String [] args){
         Room room = new Room();
-            room.setRoomNumber(3535);
-            room.setRoomType("Single");
-            room.setRoomDimension(25);
+        room.setRoomNumber(3535);
+        room.setRoomType("Single");
+        room.setRoomDimension(25);
         RoomDAO.getInstance().create(room);
 
         Customer customer = new Customer();
-            customer.setFirstName("Terry");
-            customer.setLastName("Bogard");
+        customer.setFirstName("Terry");
+        customer.setLastName("Bogard");
         CustomerDAO.getInstance().create(customer);
 
         Booking booking = new Booking();
-            booking.setArrivalDate(new Date());
-            booking.setDepartureDate(new Date());
+        booking.setArrivalDate(new Date());
+        booking.setDepartureDate(new Date());
         BookingDAO.getInstance().create(booking);
 
         System.out.println(RoomDAO.getInstance().read(3).getRoomNumber());
